8c00c92c2ccfc8019dbb4532366e39523789ffe5
arh91 <alvaro_rh_991@hotmail.com>
2022-11-29 22:57:12 +0100

    Actualizando segunda @Query.

--- app/src/main/java/com/example/simondice/UsuarioDao.kt
+++ app/src/main/java/com/example/simondice/UsuarioDao.kt
@@ -18,8 +18,8 @@ interface UsuarioDao {
     @Query("SELECT * FROM Usuario")
     fun getAll(): List<Usuario>
 
-    @Query("SELECT * FROM Usuario WHERE nick IN (:idsUsuarios)")
-    fun loadAllByIds(idsUsuarios: IntArray): List<Usuario>
+    @Query("SELECT * FROM Usuario WHERE nick IN (:nicksUsuarios)")
+    fun loadAllByIds(nicksUsuarios: Array<String>): List<Usuario>
 
     @Query("SELECT * FROM Usuario WHERE nombre LIKE :first AND primer_Apellido LIKE :last LIMIT 1")
     fun findByName(first: String, last: String, usuario:Usuario)
