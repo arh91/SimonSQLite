ef5f48ec28323748631e14c465f9d49a2353ffff
arh91 <alvaro_rh_991@hotmail.com>
2022-12-12 11:24:44 +0100

    Corrijo pequeños errores en el código.

--- app/src/main/java/com/example/simondice/MyViewModel.kt
+++ app/src/main/java/com/example/simondice/MyViewModel.kt
@@ -3,14 +3,18 @@ package com.example.simondice
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import androidx.room.Room
-import com.example.prueba.SimonDatabase
+import androidx.room.*
 
 class MyViewModel (application: Application) : AndroidViewModel(application){
 
     val rondas = mutableListOf<Int>()
     val rondaliveData = MutableLiveData<MutableList<Int>>()
 
+    init {
+        rondaliveData.value = rondas
+    }
 
     fun addList(ronda: Int){
         rondas.add(ronda)
